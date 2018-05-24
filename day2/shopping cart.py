1f96e0e11af1807c6164c8d601043b7f1c256b56
tianming1818 <181075719@qq.com>
2018-05-24 08:50:44 +0800

    python2 to python3

--- day2/shopping cart.py
+++ day2/shopping cart.py
@@ -3,8 +3,9 @@
 #商品列表
 commodity = {"pen":25,"coat":340,"umbrella":30,"keyboard":120,"shoes":230}
 money = input("please input your money: ")
-print "please select  serial number of the commodity: "
-print "the commodity name and pric is: "
+money = int(money)
+print("please select  serial number of the commodity: ")
+print("the commodity name and pric is: ")
 #定义空字典存放选择的商品数量和价格
 result = {}
 while True:
@@ -12,10 +13,10 @@ while True:
     waitselect = []
     num0 = 0
     for name,price in commodity.items():
-        print "{%d}  %s: %d" %(num0,name,price)
+        print("{%d}  %s: %d" %(num0,name,price))
         waitselect.append(name)
         num0+=1
-    num = raw_input("please input commodity number or 'q' to settle accounts: ")
+    num = input("please input commodity number or 'q' to settle accounts: ")
     totalprices = 0
     #输入q去结算
     if num == 'q':
@@ -24,26 +25,27 @@ while True:
         num = int(num)
         #如果输入超出列表序列，打印无此商品
         if num >= len(waitselect):
-            print "you select commodity is not exist, please retry..."
+            print("you select commodity is not exist, please retry...")
             continue
         count = input("please input buy quantity: ")
+        count = int(count)
         #根据输入的序号定位商品名称
         seledted = waitselect[num]
         #将商品名称和选择的数量与价格存入字典
         result[seledted] = [count, commodity[seledted] * count]
         for name, nums in result.items():
-            totalprices += nums[1]
+            totalprices += int(nums[1])
         #如果所选商品总价大于资产总额，退出程序，购买失败
         if totalprices > money:
-            print "Total amount of goods is: ",totalprices
-            print "your money is %s, Lack of balance, purchase failed" % money
+            print("Total amount of goods is: "),totalprices
+            print("your money is %d, Lack of balance, purchase failed" % money)
             exit()
     #打印所选商品的总额
-    print "Total amount of goods is: ",totalprices
+    print("Total amount of goods is: %s" %totalprices)
     amount = totalprices
 #打印购物列表和商品总价
-print "This is you selected commodity bills: "
-print '{:<10}'.format('Commodity'),'{:^20}'.format('Numbers'),'{:>10}'.format('Price')
+print("This is you selected commodity bills: ")
+print('{:<10}'.format('Commodity'),'{:^20}'.format('Numbers'),'{:>10}'.format('Price'))
 for name,num2 in result.items():
-    print '{:<10}'.format(name),'{:^20}'.format(num2[0]),'{:>10}'.format(num2[1])
-print "Total amount of goods is: ",amount
+    print('{:<10}'.format(name),'{:^20}'.format(num2[0]),'{:>10}'.format(num2[1]))
+print("Total amount of goods is: ",amount)
