bb46dcad204749758e6bcbb8ac6256d9defacbff
Rose-LY <17743535393@163.com>
2018-05-24 18:33:33 +0800

    add json diff

---
+++ json_diff/json_compare.py
@@ -0,0 +1,76 @@
+# coding:utf-8
+
+import json,time,sys
+
+
+
+def key_value(key,value):
+    '''
+    对象值比较
+    '''
+    if key == value:
+        return True
+    else:
+        return False
+
+def diff_type(a,b):
+    '''
+    对象类型比较
+    '''
+    if type(a) == type(b):
+        return True
+    else:
+        return False
+
+
+
+def diff_result(data1,data2,*args):
+    for key1,values1 in data1.items():
+        if key1 in args[0]:
+            continue
+        if data1.keys() == data2.keys():
+            if key1 in data2.keys():
+                print("\033[1;34m%s字段对比成功,新值为%s\033[0m]" %(key1,data2.keys()))
+        else:
+            print("\033[1;31m字段对比失败,\n旧值为：%s,\n新值为%s\033[0m]" %(data1.keys(),data2.keys()))
+        try:
+            if diff_type(data1[key1],data2[key1]):
+                print("\033[1;34m%s的值类型对比成功：%s和新字段的值类型%s相等\033[0m]" %(key1,type(data1[key1]),type(data2[key1])))
+            else:
+                print("\033[1;31m类型对比失败：旧字段%s的值类型%s和新字段的值类型%s不相等\033[0m]" %(key1,type(data1[key1]),type(data2[key1])))
+        except KeyError as error:
+            pass
+
+        try:
+            if isinstance(data2[key1],list):
+                oldlist = data1[key1]
+                newlist = data2[key1]
+                #当值为列表，循环读取新旧两个列表中的元素
+                for old in oldlist:
+                    for new in newlist:
+                        #如果列表中的元素为字典
+                        if isinstance(old,dict) and isinstance(new,dict):
+                            #当旧数据列表中的字典元素的索引等于新数据列表中的字典元素的索引
+                            if oldlist.index(old) == newlist.index(new):
+                                #通过两个索引取出相同的位置的字典，再传入递归函数
+                                diff_result(oldlist[oldlist.index(old)], newlist[newlist.index(new)], *args)
+            elif not isinstance(data2[key1],dict):
+                if key_value(data1[key1],data2[key1]):
+                    print("\033[1;34m%s的值对比成功：和新字段的值相等，是%s\033[0m]" % (key1,data2[key1]))
+                else:
+                    print("\033[1;31m值对比失败：旧字段%s的值和新字段的值不相等,分别是\n%s \n%s\033[0m]" % (key1,values1,data2[key1]))
+        except KeyError as error:
+            pass
+        if isinstance(data1[key1],dict):
+            diff_result(data1[key1],data2[key1],*args)
+
+
+
+import result_json
+#
+data1 = result_json.mydata
+data2 = result_json.data2
+ignore = ["created","modified","uuid"]
+#
+diff_result(data1,data2,ignore)
+
---
+++ json_diff/result_json.py
@@ -0,0 +1,208 @@
+# coding:utf-8
+
+true = True
+false = False
+null = None
+
+mydata = {
+    "timestamp": 1526899662905,
+    "duration": 10,
+    "organization": {
+        "created": null,
+        "name": "1101180419250780",
+        "modified": null,
+        "uuid": "62fd2c5a-43b9-11e8-a885-e74c3aa28f11",
+        "properties": [{"test1": 4,
+                        "test2": "wang",
+                        "test3": 3
+                        },
+                ],
+        "users": {
+            "qwe@qq.com": {
+                "applicationId": "00000000-0000-0000-0000-000000000001",
+                "username": "qwe@qq.com",
+                "name": "qwe@qq.com",
+                "email": "qwe@qq.com",
+                "activated": false,
+                "confirmed": false,
+                "created": 1526443366557,
+                "modified": 1526443366557,
+                "properties": {
+                    "admin_user_role": "admin",
+                    "category": "admin_append",
+                    "companyName": "Easemob-demo666",
+                    "telephone": "13829635271"
+                },
+                "adminUser": true,
+                "uuid": "fdd6b4da-58bd-11e8-b871-b73cc3a9c548"
+            },
+            "admin06": {
+                "applicationId": "00000000-0000-0000-0000-000000000001",
+                "username": "admin06",
+                "name": "admin06@easemob.com",
+                "email": "admin06@easemob.com",
+                "activated": true,
+                "confirmed": true,
+                "created": 1524132414310,
+                "modified": 1524132683399,
+                "properties": {
+                    "admin_user_role": "root",
+                    "companyName": "Easemob-demo666",
+                    "telephone": "13834578990"
+                },
+                "adminUser": true,
+                "uuid": "62f6006a-43b9-11e8-b7ff-1b859edb9e51"
+            },
+            "abc@qq.com": {
+                "applicationId": "00000000-0000-0000-0000-000000000001",
+                "username": "abc@qq.com",
+                "name": "abc@qq.com",
+                "email": "abc@qq.com",
+                "activated": false,
+                "confirmed": false,
+                "created": 1526443383015,
+                "modified": 1526443383015,
+                "properties": {
+                    "admin_user_role": "dev",
+                    "category": "admin_append",
+                    "companyName": "Easemob-demo666",
+                    "telephone": "13962859631"
+                },
+                "adminUser": true,
+                "uuid": "07a5ff7a-58be-11e8-a66f-bdf020d916e8"
+            }
+        },
+        "applications": {
+            "1101180419250780/test001": "e0c4d160-599e-11e8-b107-ef2e4b1f890d",
+            "1101180419250780/chat1526612733": "54168fb0-5a48-11e8-9044-a9a3ec47a53a",
+            "1101180419250780/chatdemoui": "8660d0b0-43ba-11e8-9ac7-b530dd15b195",
+            "1101180419250780/jianguo1": "f884e7b0-599c-11e8-bb04-8daf2ae1ced0",
+            "1101180419250780/chat1526615690": "367d7cf0-5a4f-11e8-8f12-c7521014eda5",
+            "1101180419250780/coco": "b505e2c0-43ba-11e8-a2b6-4d9d7c77a6f3",
+            "1101180419250780/test002": "4a159280-599f-11e8-9945-376d8c521aed",
+            "1101180419250780/chat1526612872": "a72bf0a0-5a48-11e8-8145-ab3b175e7000"
+        }
+    }
+}
+
+
+data2 = {
+    "timestamp": 1526899662905,
+    "duration": 10,
+    "organization": {
+        "created": null,
+        "name": "1101180419250555",
+        "modified": null,
+        "uuid": "62fd2c5a-43b9-11e8-a885-e74c3aa28f11",
+        "properties": [{"test1": 1,
+                        "test2": "liu",
+                        "test3": '3'
+                        },
+                ],
+        "users": {
+            "qwe@qq.com": {
+                "applicationId": "00000000-0000-0000-0000-000000000001",
+                "username": "qwe@qq.com",
+                "name": "qwe@qq.com",
+                "email": "qwe@qq.com",
+                "activated": false,
+                "confirmed": false,
+                "created": 1526443366557,
+                "modified": 1526443366557,
+                "properties": {
+                    "admin_user_role": "admin",
+                    "category": "admin_append",
+                    "companyName": "Easemob-demo666",
+                    "telephone": "13829635271"
+                },
+                "adminUser": true,
+                "uuid": "fdd6b4da-58bd-11e8-b871-b73cc3a9c548"
+            },
+            "admin06": {
+                "applicationId": "00000000-0000-0000-0000-000000000001",
+                "username": "admin06",
+                "name": "admin05@easemob.com",
+                "email": "admin06@easemob.com",
+                "activated": true,
+                "confirmed": true,
+                "created": 1524132414620,
+                "modified": 1524132683399,
+                "properties": {
+                    "admin_user_role": "root",
+                    "companyName": "Easemob-demo666",
+                    "telephone": "13834578990"
+                },
+                "adminUser": true,
+                "uuid": "62f6006a-43b9-11e8-b7ff-1b859edb9e51"
+            },
+            "abc@qq.com": {
+                "applicationId": "00000000-0000-0000-0000-000000000001",
+                "username": "abc@qq.com",
+                "name": "abc@qq.com",
+                "email": "abc@qq.com",
+                "activated": false,
+                "confirmed": false,
+                "created": 1526443383015,
+                "modified": 1526443383015,
+                "properties": {
+                    "admin_user_role": "root",
+                    "category": "admin_append",
+                    "companyName": "Easemob-demo666",
+                    "telephone": "13962855531"
+                },
+                "adminUser": true,
+                "uuid": "07a5ff7a-58be-888-a66f-bdf020d916e8"
+            }
+        },
+        "applications": {
+            "1101180419250780/test001": "e0c4d160-599e-11e8-b107-ef2e4b1f890d",
+            "1101180419250780/chat1526612733": "54168fb0-5a48-11e8-9044-a9a3ec47a53a",
+            "1101180419250780/chatdemoui": "8660d0b0-43ba-11e8-9ac7-b530dd15b195",
+            "1101180419250780/jianguo1": "f884e7b0-599c-11e8-bb04-8daf2ae1ced0",
+            "1101180419250780/chat1526612872": "a72bf0a0-5a48-11e8-8145-ab3b175e7000"
+        }
+    }
+}
+
+
+
+'''
+
+def key_value(key,value):
+
+    #对象值比较
+
+    if key == value:
+        return True
+    else:
+        return False
+
+def diff_type(a,b):
+
+    # 对象类型比较
+
+    if type(a) == type(b):
+        return True
+    else:
+        return False
+
+def diff_result(data1,data2):
+    for key1,values1 in data1.items():
+        for key2, values2 in data2.items():
+            if key_value(key1,key2):
+                print u"对比成功：旧字段%s和新字段%s相等" %(key1,key2)
+            else:
+                print u"对比失败：旧字段是%s，新字段是%s" %(key1,key2)
+            if key_value(values1,values2):
+                print u"对比成功：旧字段%s的值%s和新字段%s的值%s相等" % (key1,values1,key2,values2)
+            else:
+                print u"对比失败：旧字段%s的值是：%s；新字段%s的值是：%s" % (key1,values1,key2,values2)
+            if diff_type(data1[key1],data2[key2]):
+                print u"对比成功：旧字段%s的值类型%s和新字段%s值类型%s" %(key1,type(data1[key1]),key2,type(data2[key2]))
+            else:
+                print u"对比失败：旧字段%s的值类型是：%s，新字段%s值类型是：%s" %(key1,type(data1[key1]),key2,type(data2[key2]))
+            if isinstance(values1,dict) and isinstance(values2,dict):
+                diff_result(values1,values2)
+
+'''
+
