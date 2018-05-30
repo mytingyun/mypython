dbd06fc40dd0ff50379ff9997fc4bf552b16e8c6
Rose-LY <17743535393@163.com>
2018-05-30 18:30:17 +0800

    update day4

--- day4/myblog.py
+++ day4/myblog.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 # encoding: utf-8
-global session
-session = 0
+
+log_status = {"username": None,"status": False}
 def register():
     file1 = open('username.txt',encoding='utf-8',mode='a+')
     file1.seek(0)
     nowuser = file1.read()
-    #print('now user is:', nowuser)
     while True:
-        user = input("please input your name: ")
-        passwd1 = input("please input your passwd: ")
-        passwd2 = input("please again input your passwd: ")
+        user = input("please input your name: ").strip()
+        passwd1 = input("please input your passwd: ").strip()
+        passwd2 = input("please again input your passwd: ").strip()
         if user in nowuser:
             print("your name is exist, please again input...")
             continue
@@ -23,7 +22,8 @@ def register():
     return "%s user register success, please login..." %user
 
 
-def login(name):
+def login():
+    name = input("请输入你的用户名： ").strip()
     with open("username.txt",encoding='utf-8',mode='r+') as file2:
         alluser = file2.readlines()
     index = 0
@@ -32,13 +32,13 @@ def login(name):
         if name in user:
             reault = alluser[index]
             nowuser = reault.strip().split()
-            #print("nowuser is: ",nowuser)
             while True:
-                passwd = input("please input your password: ")
+                passwd = input("please input your password: ").strip()
                 if passwd == nowuser[1]:
                     print("login success")
-                    session = 'success'
-                    return session
+                    log_status["username"] = name
+                    log_status["status"] = True
+                    return log_status
                 else:
                     pwd+=1
                     print("your passwd is error, please retry...")
@@ -51,42 +51,55 @@ def login(name):
             break
         index += 1
 
-def article():
-    print("欢迎来到文章页面。。。。")
+def wrapper(f):
+    def inner(name,status,*args,**kwargs):
+        if name and status:
+            ret = f(name,status,*args,**kwargs)
+        else:
+            print("您尚未登陆，请先登陆。。。")
+            login()
+        return ret
+    return inner
 
-def diary():
-    print("欢迎来到日记页面。。。。")
 
-def comment():
-    print("欢迎来到评论页面。。。。")
+@wrapper
+def article(username,status):
+    print("欢迎%s来到文章页面。。。。" %username)
+@wrapper
+def diary(username,status):
+    print("欢迎%s来到日记页面。。。。" %username)
+@wrapper
+def comment(username,status):
+    print("欢迎%s来到评论页面。。。。" %username)
+@wrapper
+def enshrine(username,status):
+    print("欢迎%s来到收藏页面。。。。" %username)
 
-def enshrine():
-    print("欢迎来到收藏页面。。。。")
+
+def logout():
+    log_status["username"] = None
+    log_status["status"] = False
+    print("您已注销")
+
+
+funcs= {1: login, 2: register, 3: article, 4: diary, 5: comment, 6: enshrine, 7: logout, 8: exit}
 
 while True:
-    print("what are you doing: \n"
+    print("欢迎来到博客园首页: \n"
           "1、登陆\n"
           "2、注册\n"
-          "3、退出")
-    do = input("please select number: ")
-    do = int(do)
-    if do == 1:
-        name = input("please input your name for login: ")
-        set = login(name)
-        if set == 'success':
-            print("1、购物\n"
-                  "2、返回上级菜单")
-            select = input("please select number: ")
-            if int(select) == 1:
-                shopping()
-            elif select == 2:
-                exit()
-    elif do == 2:
-        status = register()
-        print(status)
-    elif do == 3:
-        exit()
-    else:
-        print("input error, please reatry...")
-
+          "3、文章页面\n"
+          "4、日记页面\n"
+          "5、评论页面\n"
+          "6、收藏页面\n"
+          "7、注销\n"
+          "8、退出程序")
+    selectnum = input("please select number: ").strip()
+    try:
+        if 0 < int(selectnum) <=len(funcs):
+            funcs[int(selectnum)](log_status["username"],log_status["status"])
+        else:
+            print("您输入的超出范围")
+    except ValueError as err:
+        print("您输入有误，请输入1-8的数字")
 
