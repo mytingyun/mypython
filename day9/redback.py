8f25013377f6b3670ad8decfc54e6dc2268ce22c
tianming1818 <181075719@qq.com>
2018-07-09 22:53:52 +0800

    update send red packet

--- day9/redback.py
+++ day9/redback.py
@@ -2,12 +2,26 @@
 import random
 #https://www.cnblogs.com/Eva-J/articles/9235899.html
 
-# redback = [i for i in range(201)]
-# print(redback)
-#
-# myrandom = []
+def baks(num,money):
+    rednum = [i for i in range(money)]
+    myrandom = []
+    for i in range(num-1):
+        nums = random.choice(rednum)
+        rednum.remove(nums)
+        myrandom.append(nums)
+    myrandom.sort()
 
+    if myrandom[0] != 0:
+        myrandom.insert(0,0)
+    if myrandom[-1] != money:
+        myrandom.append(money)
 
+    smallbak = []
+    while num > 0:
+        smallbak.append(myrandom[num] - myrandom[num-1])
+        num -= 1
+    print(smallbak)
 
+baks(30,200)
 
 
