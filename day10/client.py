21731c03f6e174ae28f3a949da6d04d1d4f05483
Rose-LY <17743535393@163.com>
2018-07-25 15:37:21 +0800

    updata day10

--- day10/client.py
+++ day10/client.py
@@ -1,30 +1,53 @@
 # coding: utf-8
 
+import os,time
 import socket
+import hashlib
+
+def get_md5(file,n = 10240):
+    with open(file, 'rb') as f1:
+        md5_obj = hashlib.md5()
+        file_size = os.path.getsize(file)
+        while file_size>0:
+            md5_obj.update(f1.read(n))
+            file_size -= n
+        return md5_obj.hexdigest()
 
 
 
+def cli_conn(file,n=5000):
+    cli = socket.socket()
+    cli.connect(('127.0.0.1',9001))
+    while True:
+        print("请可以操作：\n"
+              "1、上传\n"
+              "2、下载\n"
+              "3、退出")
+        select = input("请选择：").strip()
+        if select == '1':
+            cli.send(select.encode('utf-8'))
+            msg = cli.recv(1024).decode('utf-8')
+            print("服务器端消息：",msg)
+            # cli.send(b"uploading the file")
+            with open(file, 'rb') as f1:
+                file_size = os.path.getsize(file)
+                cli.send(file.encode('utf-8'))
+                cli.send(str(file_size).encode('utf-8'))
+                time.sleep(0.1)
+                while file_size > 0:
+                    content = f1.read(n)
+                    print("读取类型：",type(content))
+                    cli.send(content)
+                    file_size -= n
+        elif select == '2':
+            cli.send(select.encode('utf-8'))
+            msg2 = cli.recv(1024).decode('utf-8')
+            print("服务器端消息：", msg2)
+            cli.send(b'I want download the files')
+        elif select == '3':
+            cli.send(select.encode('utf-8'))
+            break
+    cli.close()
 
-cli = socket.socket()
-cli.connect(('127.0.0.1',9001))
-while True:
-    print("请可以操作：\n"
-          "1、上传\n"
-          "2、下载\n"
-          "3、退出")
-    select = input("请选择：").strip()
-    if select == '1':
-        cli.send(select.encode('utf-8'))
-        msg = cli.recv(1024).decode('utf-8')
-        print("服务器端消息：",msg)
-        cli.send(b"uploading the file")
-    elif select == '2':
-        cli.send(select.encode('utf-8'))
-        msg2 = cli.recv(1024).decode('utf-8')
-        print("服务器端消息：", msg2)
-        cli.send(b'I want download the files')
-    elif select == '3':
-        cli.send(select.encode('utf-8'))
-        break
-cli.close()
-
+if __name__ == '__main__':
+    cli_conn('sea_sky')
--- day10/server.py
+++ day10/server.py
@@ -1,28 +1,57 @@
 # coding: utf-8
-
+import os
 import socket
+import hashlib
 
-serv = socket.socket()
+files = {'filename':'receive','filesize':0}
 
-serv.bind(('127.0.0.1',9001))
-serv.listen()
-while True:
-    conn,addr = serv.accept()
+# def rece_file(filename,contact):
+#     with open(filename,mode='ab') as f1:
+#         f1.write(contact)
+
+
+
+def serve_start():
+    serv = socket.socket()
+    serv.bind(('127.0.0.1',9001))
+    serv.listen()
     while True:
-        res = conn.recv(1024)
-        #print("res is:",res)
-        if res == b'1':
-            conn.send(b"please upload")
+        conn,addr = serv.accept()
+        while True:
             res = conn.recv(1024)
-            print("客户端端消息：", res)
-        if res == b'2':
-            conn.send(b"ple select download filename")
-            res = conn.recv(1024)
-            print("客户端端消息：", res)
-        if res == b'3':
-            break
-    conn.close()
-serv.close()
+            #print("res is:",res)
+            if res == b'1':
+                conn.send(b"please upload")
+                filename = conn.recv(7)
+                file1 = filename.decode('utf-8')
+                files['filename'] = file1+'back'
+                print('文件名是：',files['filename'])
+                filesize = conn.recv(1024)
+                files['filesize'] = filesize
+                print("文件大小是：",files['filesize'],type(files['filesize']))
+                files['filesize'] = int(files['filesize'])
+                f1 = open(files['filename'], mode='wb')
+                while files['filesize'] > 0:
+                    content = conn.recv(5000)
+                    # print("内容是：",content,"类型：",type(content))
+                    # content = content.decode('utf-8')
+                    # rece_file(files['filename'],content)
+                    f1.write(content)
+                    files['filesize'] -= 5000
+                f1.close()
+            if res == b'2':
+                conn.send(b"ple select download filename")
+                res = conn.recv(1024)
+                print("客户端端消息：", res)
+            if res == b'3':
+                break
+        conn.close()
+    serv.close()
+
+
+if __name__ == '__main__':
+    serve_start()
+
 
 
 
