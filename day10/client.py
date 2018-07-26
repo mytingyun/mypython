cba69a0175ef13835ec94194452d337bd42a59ed
Rose-LY <17743535393@163.com>
2018-07-26 16:25:50 +0800

    update day10

--- day10/client.py
+++ day10/client.py
@@ -4,19 +4,11 @@ import os,time
 import socket
 import hashlib
 
-def get_md5(file,n = 10240):
-    with open(file, 'rb') as f1:
-        md5_obj = hashlib.md5()
-        file_size = os.path.getsize(file)
-        while file_size>0:
-            md5_obj.update(f1.read(n))
-            file_size -= n
-        return md5_obj.hexdigest()
-
 
 def cli_conn(n=1024):
+    md5_obj = hashlib.md5()
     cli = socket.socket()
-    cli.connect(('127.0.0.1',9001))
+    cli.connect(('127.0.0.1',9002))
     while True:
         print("请可以操作：\n"
               "1、上传\n"
@@ -24,20 +16,33 @@ def cli_conn(n=1024):
               "3、退出")
         select = input("请选择：").strip()
         if select == '1':
+            #发送1到服务器端
             cli.send(select.encode('utf-8'))
-            file1 = input("请输入要上传的文件名：")
             msg = cli.recv(1024).decode('utf-8')
             print("服务器端消息：",msg)
+            file1 = input("请输入要上传的文件名：")
             with open(file1, 'rb') as f1:
                 file_size = os.path.getsize(file1)
+                #获取文件大小并发送文件名给服务端
                 cli.send(file1.encode('utf-8'))
+                time.sleep(0.5)
+                #发送文件大小给服务端
                 cli.send(str(file_size).encode('utf-8'))
                 time.sleep(0.5)
+                #开始发送文件并获取md5值
                 while file_size > 0:
                     content = f1.read(n)
+                    md5_obj.update(content)
                     cli.send(content)
                     file_size -= n
+                f1.close()
+            time.sleep(1)
+            #将本地获得的文件的md5值发送到服务器端
+            md5s = md5_obj.hexdigest().encode('utf-8')
+            cli.send(md5s)
+            print('本文件md5值是：',md5s)
         elif select == '2':
+            # 发送2到服务器端
             cli.send(select.encode('utf-8'))
             msg2 = cli.recv(1024).decode('utf-8')
             print("服务器端消息：", msg2)
@@ -50,10 +55,17 @@ def cli_conn(n=1024):
             f1 = open(file2, mode='wb')
             while filesize > 0:
                 content = cli.recv(1024)
+                md5_obj.update(content)
                 f1.write(content)
                 filesize -= 1024
             f1.close()
-            # cli.send(b'I want download the files')
+            remote_md5 = cli.recv(1024)
+            local_md5 = md5_obj.hexdigest()
+            if remote_md5.decode('utf-8') == local_md5:
+                print("发送端的md5值和本地接收后的md5值相等，都是:", local_md5)
+            else:
+                print("发送端的md5是:", remote_md5)
+                print("接收后的文件md5是：", local_md5)
         elif select == '3':
             cli.send(select.encode('utf-8'))
             break
--- day10/server.py
+++ day10/server.py
@@ -5,36 +5,48 @@ import hashlib
 
 files = {'filename':'receive','filesize':0}
 
-# def rece_file(filename,contact):
-#     with open(filename,mode='ab') as f1:
-#         f1.write(contact)
 
 def serve_start(n=1024):
+    md5_obj = hashlib.md5()
     serv = socket.socket()
-    serv.bind(('127.0.0.1',9001))
+    serv.bind(('127.0.0.1',9002))
     serv.listen()
     while True:
         conn,addr = serv.accept()
         while True:
             res = conn.recv(1024)
-            #print("res is:",res)
+            #根据客户端发送的消息进行选择上传还是下载
             if res == b'1':
                 conn.send(b"please upload")
+                #接收文件名
                 filename = conn.recv(1024)
-                time.sleep(0.1)
+                time.sleep(0.2)
                 file1 = filename.decode('utf-8')
                 files['filename'] = file1+'back'
                 print('文件名是：',files['filename'])
+                #接收文件大小
                 filesize = conn.recv(1024)
+                time.sleep(0.2)
                 files['filesize'] = filesize
                 print("文件大小是：",files['filesize'])
                 files['filesize'] = int(files['filesize'])
-                f1 = open(files['filename'], mode='wb')
-                while files['filesize'] > 0:
-                    content = conn.recv(1024)
-                    f1.write(content)
-                    files['filesize'] -= 1024
-                f1.close()
+                with open(files['filename'], mode='wb') as f1:
+                    while files['filesize'] > 0:
+                        #开始接收文件并计算文件md5值
+                        content = conn.recv(1024)
+                        md5_obj.update(content)
+                        f1.write(content)
+                        files['filesize'] -= 1024
+                #接收客户端的md5值
+                remote_md5 = conn.recv(1024)
+                #算出本地md5值
+                local_md5 = md5_obj.hexdigest()
+                #将本地计算的md5值和客户端值的md5值进行比较
+                if remote_md5.decode('utf-8') == local_md5:
+                    print("发送端的md5值和本地接收后的md5值相等，都是:",local_md5)
+                else:
+                    print("发送端的md5是:", remote_md5)
+                    print("接收后的文件md5是：", local_md5)
             if res == b'2':
                 conn.send(b"ple select download filename")
                 file2 = conn.recv(1024)
@@ -44,11 +56,16 @@ def serve_start(n=1024):
                     file_size = os.path.getsize(file2)
                     print('file size is',file_size)
                     conn.send(str(file_size).encode('utf-8'))
-                    time.sleep(0.1)
+                    time.sleep(1)
                     while file_size > 0:
                         content = f2.read(n)
+                        md5_obj.update(content)
                         conn.send(content)
                         file_size -= n
+                time.sleep(1)
+                md5s = md5_obj.hexdigest().encode('utf-8')
+                conn.send(md5s)
+                print("本地文件md5值是：",md5s)
             if res == b'3':
                 break
         conn.close()
@@ -59,15 +76,3 @@ if __name__ == '__main__':
     serve_start()
 
 
-
-
-# import os
-# import hashlib
-# def get_md5(file,n = 10240):
-#     with open(file, 'rb') as f1:
-#         md5_obj = hashlib.md5()
-#         file_size = os.path.getsize(file)
-#         while file_size>0:
-#             md5_obj.update(f1.read(n))
-#             file_size -= n
-#         return md5_obj.hexdigest()
